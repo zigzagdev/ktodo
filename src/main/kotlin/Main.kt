6b3dbbd1e8b0b23ef67dfb44b14ad74aa6e09733
zigzagdev <msts.oo0131@gmail.com>
2023-11-10 01:42:00 +0900

    todobase-make

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,6 +5,4 @@ fun main(args: Array<String>) {
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 
-
-
 }
